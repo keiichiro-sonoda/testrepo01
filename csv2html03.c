ec4a0908261853cf87a93696b79a97a9569ccca8
keiichiro-sonoda <mountain13577749@gmail.com>
2021-05-13 13:20:23 +0900

    ファイルに直接書き込んでいくバージョンもできた?

--- csv2html03.c
+++ csv2html03.c
@@ -4,8 +4,6 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define HTML_MAX 1024  // HTML文字列の最大長
-
 // HTMLの冒頭の文字列
 #define HTML_HEAD "<html><table border=1><tr><td>"
 
@@ -52,11 +50,9 @@ int main(int argc, char **argv) {
         puts("\a引数にcsvファイルのパスを1つ指定してください。");
         return -1;
     }
-    FILE *fpr, *fpw;                          // ファイルのポインタ
+    FILE *fpr, *fpw;                   // ファイルのポインタ
     char c;                            // 読み込む文字
     char fnamer[FILENAME_MAX], fnamew[FILENAME_MAX];  // ファイル名 (入力, 出力)
-    char html[HTML_MAX] = HTML_HEAD;   // 出力文字列 (冒頭はマクロで定義)
-    size_t html_len = strlen(html);    // 出力文字列の長さ (ナル文字を含まない)
     int dq_open = 0;                   // ダブルクォートが開いているフラグ
     int dq_first = 1;                  // 最初のダブルクォートかどうか
     int new_line = 0;                  // 改行直後
@@ -74,17 +70,23 @@ int main(int argc, char **argv) {
         printf("\a%s can\'t be opened.\n", fnamew);
         return -1;
     }
+    fprintf(fpw, HTML_HEAD); // 冒頭の文字列の書込み
     // ファイルの終わりまで繰り返し
     while ((c = getc(fpr)) != EOF) {
         if (new_line) { // 改行直後がナル文字なら, 最終行にカウントしない
             if (c == '\0') break;
-            else html_len = addMoji(html, "</td></tr><tr><td>", HTML_MAX);
-            new_line = 0;
+            else {      // ナル文字以外なら普通に改行
+                fprintf(fpw, "</td></tr><tr><td>");
+            }
+            new_line = 0; // フラグを下ろす
         }
         if (dq_open) { // ダブルクォートが開いている場合
             if (c == '"') dq_open = 0; // ダブルクォートが出たら閉じる
-            else if (c == '\n') html_len = addMoji(html, "<br>", HTML_MAX); // 改行が出たら <br> に置換
-            else html[html_len++] = c; // それ以外は全て文字として扱う
+            else if (c == '\n') {
+                fprintf(fpw, "<br>");  // 改行が出たら <br> に置換
+            } else {
+                fputc(c, fpw);         // それ以外は全て文字として扱う
+            }
         }
         else { // ダブルクォートが開いていない場合
             if (c == '"') { // ダブルクォートが出たら開く
@@ -92,36 +94,21 @@ int main(int argc, char **argv) {
                 if (dq_first) {   // 最初のダブルクォートの場合
                     dq_first = 0; // フラグを下ろす
                 } else {                  // 最初のダブルクォートでない場合
-                    html[html_len++] = c; // ダブルクォートを文字として扱う
+                    fputc('"', fpw);      // ダブルクォートを文字として扱う
                 }
-            } else if (c == ',') { // カンマが出たら要素を区切る
-                html_len = addMoji(html, "</td><td>", HTML_MAX);
+            } else if (c == ',') {  // カンマが出たら要素を区切る
+                fprintf(fpw, "</td><td>");
                 dq_first = 1;       // フラグリセット
             } else if (c == '\n') { // 改行
                 new_line = 1;       // 改行フラグを立てる
             } else { // それ以外
-                html[html_len++] = c;
+                fputc(c, fpw);
             }
-        }
-        // 出力文字列の長さがオーバーした場合
-        if (html_len >= HTML_MAX) {
-            printf("\a長さオーバー: %lu\n", html_len);
-            html[HTML_MAX - 1] = '\0';
-            puts(html);
-            return -1;
         }
     }
     // 最後の文字列を追加
-    if ((html_len = addMoji(html, "</td></tr></table></html>", HTML_MAX)) >= HTML_MAX) {
-        printf("\a長さオーバー: %lu\n", html_len);
-        html[HTML_MAX - 1] = '\0';
-        puts(html);
-        return -1;
-    }
-    puts(html);
-    // 長さチェック
-    printf("HTMLの長さ: %ld \n", html_len);
-    fprintf(fpw, "%s", html);  // ファイルに書き込む
+    fprintf(fpw, "</td></tr></table></html>");
+
     fclose(fpr);               // 読込み用ファイルを閉じる
     fclose(fpw);               // 書込み用ファイルを閉じる
     return 0;
