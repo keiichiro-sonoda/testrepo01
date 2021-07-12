c60eeb0477c8d78a8130423273efb77dce406713
keiichiro-sonoda <mountain13577749@gmail.com>
2021-07-12 12:42:24 +0900

    入れ替えマクロを変更

--- bubble_sort.c
+++ bubble_sort.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define swapArray(type, A, i, j) do {\
-    type _tmp;\
-    _tmp = A[i];\
-    A[i] = A[j];\
-    A[j] = _tmp;\
+// 配列の要素を入れ替えるマクロ
+// 一時変数は呼び出し元で用意しておく
+#define swapArray(A, i, j, tmp) do {\
+    (tmp) = (A)[i];\
+    (A)[i] = (A)[j];\
+    (A)[j] = (tmp);\
 } while (0)
 
 // 配列表示関数
@@ -18,11 +19,11 @@ void showIntArray(const int *A, int n) {
 
 // バブルソート関数
 void bubbleSort(int *A, int n) {
-    int i, j;
+    int i, j, tmp;
     for (i = 0; i < n - 2; i++) {
         for (j = i; j < n - 2; j++) {
             if (A[j] > A[j + 1]) {
-                swapArray(int, A, j, j + 1);
+                swapArray(A, j, j + 1, tmp);
             }
         }
     }
