05b3cad1a9a7fae48d6c8a5336112fd4edf80d5b
keiichiro-sonoda <mountain13577749@gmail.com>
2021-07-25 12:43:08 +0900

    一応ちゃんとデコードできてるっぽい まだノイズ乗せてないからわからんけど

--- test01.c
+++ test01.c
@@ -14,16 +14,31 @@ void printBin32(u_int x) {
 }
 
 // 繰り返し符号を生成して返す
-u_int encRepCode3(u_int m, int n) {
+u_int encRepCode3(u_int msg, int nm) {
     u_int code = 0;
     int i, j, b;
-    for (i = 0; i < n; i++) {
-        b = getBit(m, i);
+    for (i = 0; i < nm; i++) {
+        b = getBit(msg, i);
         for (j = 0; j < 3; j++) {
             code |= b << (i * 3 + j);
         }
     }
     return code;
+}
+
+u_int decRepCode3(u_int code, int nm) {
+    u_int msg = 0;
+    int i, j, s;
+    for (i = 0; i < nm; i++) {
+        s = 0;
+        for (j = 0; j < 3; j++) {
+            s += getBit(code, i * 3 + j);
+        }
+        if (s >= 2) {
+            msg |= 1 << i;
+        }
+    }
+    return msg;
 }
 
 int main(void) {
@@ -34,5 +49,6 @@ int main(void) {
     printBin32(m);
     c = encRepCode3(m, 4);
     printBin32(c);
+    printBin32(decRepCode3(c, 4));
     return 0;
 }
