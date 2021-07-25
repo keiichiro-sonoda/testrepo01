7b55631df0bbd9c8408533ae31270720adbb3a38
keiichiro-sonoda <mountain13577749@gmail.com>
2021-07-25 12:18:42 +0900

    マクロgetBitを作成

--- test01.c
+++ test01.c
@@ -2,11 +2,27 @@
 #include <stdlib.h>
 #include <time.h>
 
-void printBin32(u_int n) {
+// 指定したビットを取得
+#define getBit(x, n) (((x) >> (n)) & 1)
+
+// 32ビットのバイナリ表示
+void printBin32(u_int x) {
     for (int i = 31; i >= 0; i--) {
-        printf("%d", (n >> i) & 1);
+        printf("%d", getBit(x, i));
     }
     putchar(10);
+}
+
+// 繰り返し符号を生成して返す
+u_int repCode3(u_int m, int n) {
+    u_int code = 0;
+    // int i, j;
+    // for (i = 0; i < n; i++) {
+    //     for (j = 0; j < 3; j++) {
+
+    //     }
+    // }
+    return code;
 }
 
 int main(void) {
