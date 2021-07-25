a41f6d2851acb90b32bf75dce891b6abb1ff0ec4
keiichiro-sonoda <mountain13577749@gmail.com>
2021-07-25 12:09:08 +0900

    乱数表示してみた

--- test01.c
+++ test01.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 int main(void) {
-    puts("こんにちは");
+    // puts("こんにちは");
+    srand((unsigned)time(NULL));
+    printf("%d\n", rand());
     return 0;
 }
