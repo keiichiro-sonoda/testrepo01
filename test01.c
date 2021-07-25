3ca81767b3bbdc99c9a379f13c3f372f4248a8f3
keiichiro-sonoda <mountain13577749@gmail.com>
2021-07-25 12:13:12 +0900

    生成するのは多分31ビットの乱数らしい 先頭ビットは符号だから

--- test01.c
+++ test01.c
@@ -2,9 +2,19 @@
 #include <stdlib.h>
 #include <time.h>
 
+void printBin32(u_int n) {
+    for (int i = 31; i >= 0; i--) {
+        printf("%d", (n >> i) & 1);
+    }
+    putchar(10);
+}
+
 int main(void) {
     // puts("こんにちは");
     srand((unsigned)time(NULL));
-    printf("%d\n", rand());
+    int r = rand();
+    printf("%d\n", r);
+    printBin32(r);
+    // printBin32(8);
     return 0;
 }
